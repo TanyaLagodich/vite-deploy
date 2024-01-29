71a3d71c01754f66f3665e4b8b2f47a149367af2
Tatiana Lagodich <t.lagodich@amarkets.com>
2024-01-29 16:48:36 +0400

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/vite-deploy/,
     build: {
         outDir: 'dist', // Каталог, в который будет помещен результат сборки
         assetsDir: 'assets', // Подкаталог, куда будут помещены статические ресурсы
